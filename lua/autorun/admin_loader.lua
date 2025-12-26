a2ddff6d71ca0c6c3624d825b007d3df9d355bae
Dark <32241100+Dahhrk@users.noreply.github.com>
2025-12-26 20:55:23 +0000

    Delete admin_loader.lua

--- lua/autorun/admin_loader.lua
+++
@@ -1,5 +0,0 @@
--- autorun/admin_loader.lua
-if SERVER then
-    include("admin_system/init.lua") -- Load the main admin system entry point
-    print("[Sovereign-Interface] Admin system initialized.")
-end
